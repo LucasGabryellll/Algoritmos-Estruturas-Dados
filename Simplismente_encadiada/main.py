aca9ffdfe5a4342a43c37aefbcdb5ca8d5eac51e
Lucas Gabryel <LucasGabryellll>
2020-03-29 12:56:04 -0300

    Lista simplismente encadeada

---
+++ Simplismente_encadiada/main.py
@@ -0,0 +1,11 @@
+from Simplismente_encadiada.Lista import Lista
+
+lista = Lista()
+print(lista)
+lista.add('10')
+lista.add('20')
+lista.add('30')
+print(lista)
+lista.addIndex(0, '11')
+lista.addIndex(3, '50')
+print(lista)
